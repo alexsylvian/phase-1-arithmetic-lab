872d867bd98e8774b4042b2880731b3769edb095
Alexander <lazeratus1@gmail.com>
2023-06-07 18:28:11 -0400

    math functions as variables (for some reason)

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+const multiply = num1 * num2
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 16
+const num4 = 4
+const mod = 16/4 
+
+const max = Math.max(1, 2, 3, 4, 5, 20);
